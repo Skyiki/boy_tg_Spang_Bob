f42bbe4af9c2ae5d0d130eb1c3b538517a53bad7
Barserv <taisiyabum@gmail.com>
2023-12-10 18:39:12 +0300

    начальная версия

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ import telebot
 token = '6682935044:AAEnE2y8KHraymUKeaxulz-4p0l5BWfcyhg'
 bot = telebot.TeleBot(token=token)
 
+function = ["start", "help", '']
+
+function_description = {
+    "start": "Начало работы бота",
+    "help": "Справочная информация о доступных командах и их функциональности."
+}
+
+def functions():
+    for i in range(len(function_description)):
+        print(i + 1, '-', function_description[i])
+
+
 @bot.message_handler(commands=['start'])
 def start_func(message):
-    bot.send_message(chat_id=message.chat.id, text= f'Это бот визитка для всем известного персонажа - Губку Боба! \n'
+    bot.send_message(chat_id=message.chat.id, text= f'Это бот визитка для известного персонажа - Губку Боба! \n'
                                                     f'Можешь ознакомиться с доступными командами с помощью команды /help'
                      )
 
-def help
+@bot.message_handler(commands=['help'])
+def help_func(message):
+    bot.send_message(chat_id=message.chat.id, text= f'У этого бота присутвуют команды: {function_description}')
+
+bot.infinity_polling()
