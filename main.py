dcca449ddb56e2e2fee91ffbda5c4df1c4eb97e5
Barserv <taisiyabum@gmail.com>
2023-12-10 19:44:54 +0300

    версия только с коммандами.

--- main.py
+++ main.py
@@ -3,26 +3,62 @@ import telebot
 token = '6682935044:AAEnE2y8KHraymUKeaxulz-4p0l5BWfcyhg'
 bot = telebot.TeleBot(token=token)
 
-function = ["start", "help", '']
+function = ["start", "help", 'hobby', 'aboutme']
 
 function_description = {
     "start": "Начало работы бота",
-    "help": "Справочная информация о доступных командах и их функциональности."
+    "help": "Справочная информация о доступных командах и их функциональности.",
+    "hobby": "Рассказывает про свои увлечения, работу, хобби.",
+    "aboutme": "Рассказывает про себя."
 }
 
-def functions():
-    for i in range(len(function_description)):
-        print(i + 1, '-', function_description[i])
+
+# def functions():
+#    for i in range(len(function_description)):
+#        pp = pprint.PrettyPrinter(indent=2)
+#        pp.pprint(function_description)
 
 
 @bot.message_handler(commands=['start'])
 def start_func(message):
-    bot.send_message(chat_id=message.chat.id, text= f'Это бот визитка для известного персонажа - Губку Боба! \n'
-                                                    f'Можешь ознакомиться с доступными командами с помощью команды /help'
+    bot.send_message(chat_id=message.chat.id,
+                     text=f'В Бикини Боттом новый день, и я, Губка Боб, здесь, чтобы сделать его особенным! \n'
+                          f'Хочешь узнать, что я могу? Тогда нажимай - /help'
                      )
+
 
 @bot.message_handler(commands=['help'])
 def help_func(message):
-    bot.send_message(chat_id=message.chat.id, text= f'У этого бота присутвуют команды: {function_description}')
+    bot.send_message(chat_id=message.chat.id, text=f"Я умею: start - запуск бота."
+                                                   "help - справочная информация о доступных командах и их "
+                                                   "функциональности."
+                                                   "hobby - рассказывает про свои увлечения, работу, хобби."
+                                                   "aboutme - рассказывает про себя."
+                     )
+
+
+@bot.message_handler(commands=['hobby'])
+def help_func(message):
+    bot.send_message(chat_id=message.chat.id, text=f'    Я обожаю ловить медуз с моим другом Патриком.'
+                                                   f' Это наше общее хобби, которое мы оба очень любим.'
+                                                   f' Мы проводим много времени на пляже, пытаясь поймать как '
+                                                   f'можно больше медуз.\n'
+                                                   f'Иногда мы даже соревнуемся друг с другом, кто поймает '
+                                                   f'больше медуз за определенное время!')
+
+@bot.message_handler(commands=['aboutme'])
+def aboutme_func(message):
+    bot.send_message(chat_id=message.chat.id, text=f'Я работаю в Красти Краб, лучшем ресторане на свете, где я '
+                                                   f'готовлю вкуснейшие крабсбургеры для своих любимых клиентов.\n'
+                                                   f'Моя жизнь полна приключений, забавных ситуаций и самых верных '
+                                                   f'друзей!'
+                                                   f'Мой лучший друг - это, конечно же, Патрик Стар, глуповатый, '
+                                                   f'но невероятно милый и забавный звезда.'
+                                                   f' А еще у меня есть Сквидвард, сосед и коллега по работе, который '
+                                                   f'постоянно ворчит и ругает меня, но на самом деле он очень добрый '
+                                                   f'и заботливый.'
+                                                   f'Я люблю свою работу и свой город, и каждый день я узнаю что-то '
+                                                   f'новое и интересное. Бикини Боттом - это место, где возможно все, '
+                                                   f'и я всегда готов к новым приключениям и безумным выходкам!')
 
 bot.infinity_polling()
